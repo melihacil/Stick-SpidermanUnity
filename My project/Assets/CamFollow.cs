dbda3ed87e607f7fa45b7f115093d6d6e0cb50e1
melihacil <knghtmellomello@gmail.com>
2022-09-05 14:40:31 +0300

    Update

--- My project/Assets/CamFollow.cs
+++ My project/Assets/CamFollow.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
+
+    [SerializeField]private Transform player;
+
+
+    private Vector3 offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - player.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, transform.position + offset, Time.deltaTime * 3);
     }
 }
